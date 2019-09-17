c4e2c980e5fc3507bbc1f284762f708136e86d81
Dustin Frisch <fooker@lab.sh>
2019-09-17 22:52:32 +0200

    NMS-12185: Ignore SFlow messages without IP data
    SFlow is capable of sending samples about other network layers beside
    IP. To make a usefull flow out of the sample, the IP data is required.
    Flows not containing IP related information are silently ignored now.

--- features/telemetry/protocols/common/src/main/java/org/opennms/netmgt/telemetry/protocols/common/utils/BsonUtils.java
+++ features/telemetry/protocols/common/src/main/java/org/opennms/netmgt/telemetry/protocols/common/utils/BsonUtils.java
@@ -32,6 +32,8 @@ import java.time.Instant;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import org.bson.BsonArray;
+import org.bson.BsonBoolean;
 import org.bson.BsonDocument;
 import org.bson.BsonDouble;
 import org.bson.BsonInt32;
@@ -70,6 +72,18 @@ public class BsonUtils {
 
     public static Optional<String> getString(final BsonDocument doc, final String... path) {
         return get(doc, path).map(BsonValue::asString).map(BsonString::getValue);
+    }
+
+    public static Optional<Boolean> getBool(final BsonDocument doc, final String... path) {
+        return get(doc, path).map(BsonValue::asBoolean).map(BsonBoolean::getValue);
+    }
+
+    public static Optional<Iterable<BsonValue>> getArray(final BsonDocument doc, final String... path) {
+        return get(doc, path).map(BsonValue::asArray).map(BsonArray::getValues);
+    }
+
+    public static Optional<BsonDocument> getDocument(final BsonDocument doc, final String... path) {
+        return get(doc, path).map(BsonValue::asDocument);
     }
 
     public static <V> Optional<V> first(final Optional<V>... values) {
